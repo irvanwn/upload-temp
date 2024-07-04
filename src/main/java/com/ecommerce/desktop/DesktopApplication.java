4c04d2b2d74a39d96908284a17dd0839f597a3c4
selsun <nicholasirvan2112@gmail.com>
2024-07-04 22:49:24 +0700

    change ip

--- src/main/java/com/ecommerce/desktop/DesktopApplication.java
+++ src/main/java/com/ecommerce/desktop/DesktopApplication.java
@@ -2,6 +2,9 @@ package com.ecommerce.desktop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DesktopApplication {
@@ -10,4 +13,19 @@ public class DesktopApplication {
 		SpringApplication.run(DesktopApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("product_route", r -> r.path("/api/products/**")
+						.uri("http://localhost:8081"))
+				.route("store_route", r -> r.path("/api/stores/**")
+						.uri("http://localhost:8082"))
+				.route("cart_route", r -> r.path("/api/cart/**")
+						.uri("http://localhost:8083"))
+				.route("shipping_route", r -> r.path("/api/shipping/**")
+						.uri("http://localhost:8084"))
+				.route("transaction_route", r -> r.path("/api/transaction/**")
+						.uri("http://localhost:8085"))
+				.build();
+	}
 }
